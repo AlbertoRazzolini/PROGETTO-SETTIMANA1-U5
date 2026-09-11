fa81f3d8947e745ea5009708496e8cec8bfd6499
AlbertoRazzolini <alrazzo2@libero.it>
2026-09-11 10:08:03 +0200

    Start FE BE

---
+++ BE/src/main/java/com/example/progettosettimana1u5/ProgettoSettimana1U5Application.java
@@ -0,0 +1,13 @@
+package com.example.progettosettimana1u5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana1U5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimana1U5Application.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/example/progettosettimana1u5/ProgettoSettimana1U5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettosettimana1u5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimana1U5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
